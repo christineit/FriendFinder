c036241a44e02ef603182d1d177b3183f6d29444
Christine <christineitrinh@gmail.com>
2019-04-06 23:24:16 -0500

    testing html routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,56 +1,50 @@
 // require mysql
 // connection info
-// create the connection 
+// create the connection
 
 let friends;
 
 //declare functions for querying the db and a finding match
 function getProfiles() {
-    connection.query(get all the profiles)
-    friends = profiles
+  connection.query("SELECT * FROM profiles");
+  friends = profiles;
 }
-
 
 //finding a match
 
-function findMatch(userData, friends) {
-    const bestMatch = {
-        name: '',
-        photo: 'url',
-        friendDifference: Infinity
-    }
+// function findMatch(userData, friends) {
+//     const bestMatch = {
+//         name: '',
+//         photo: 'url',
+//         friendDifference: Infinity
+//     }
 
-    for (let i loop over the friends) {
-        totalDifference = 0;
-        currentFriend = friends[i]
+//     for (let i loop over the friends) {
+//         totalDifference = 0;
+//         currentFriend = friends[i]
 
-        for (let j loop over currentFriend.scores){
-            //compare the userData.scores[j] and currentFriend.scores[j]
+//         for (let j loop over currentFriend.scores){
+//             //compare the userData.scores[j] and currentFriend.scores[j]
 
-            totalDifference += Math.abs(parseInt(user score) - parseInt(friend score))
-        }
+//             totalDifference += Math.abs(parseInt(user score) - parseInt(friend score))
+//         }
 
-if (totalDifference <= bestMatch.friendDifference) {
-            bestMatch = currentFriend
-        }
+// if (totalDifference <= bestMatch.friendDifference) {
+//             bestMatch = currentFriend
+//         }
 
-        
-    }
+//     }
 
-return bestMatch;
-}
+// return bestMatch;
+// }
 
-}
+// }
 
-
-
-module.exoprts = {
-    //get route for all profiles
-
-    //post route to add new profiles
-    //when posting new user data, also use that data to find match
-    //response with matched profile
-
-    const friends = getFriends()
-    findMatch(userData, friends)
-}
+module.exports = {
+  //get route for all profiles
+  //post route to add new profiles
+  //when posting new user data, also use that data to find match
+  //response with matched profile
+  // const friends = getFriends()
+  // findMatch(userData, friends)
+};
